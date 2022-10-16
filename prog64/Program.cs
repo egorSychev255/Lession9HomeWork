7260b3da7b549ce92de853ade3490e7d833b7c69
Egor <egorca255@mail.ru>
2022-10-16 10:53:17 +0300

    Добавил описания к задачам

--- prog64/Program.cs
+++ prog64/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("ВВедите N");
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+Console.WriteLine("ВВедите N");
 int UserNumber = Convert.ToInt32(Console.ReadLine());
 WriteNumbers(UserNumber);
 
--- prog66/Program.cs
+++ prog66/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите M");
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.WriteLine("Введите M");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите N");
  int n = Convert.ToInt32(Console.ReadLine());
