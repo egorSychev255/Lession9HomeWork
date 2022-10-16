ef8aa22cc6fcc151619b71131844019bc58df122
Egor <egorca255@mail.ru>
2022-10-16 10:50:54 +0300

    push

---
+++ prog64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите N");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+WriteNumbers(UserNumber);
+
+void WriteNumbers(int n)
+{
+    if (n > 0)
+    {
+        Console.Write($"{n},");
+        WriteNumbers(n - 1);
+    }
+}   
---
+++ prog66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+ int n = Convert.ToInt32(Console.ReadLine());
+int temp = m;
+Console.Write($"Сумма в промежутке от {m} до  {n} равна : ");
+CalculateSummAndPrint(m, n, temp = 0);
+
+void CalculateSummAndPrint(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    CalculateSummAndPrint(m, n - 1, sum);
+
+}
